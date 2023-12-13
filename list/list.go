eb263838984b1b39604b71c684e5a2b7951be4bc
jgsheppa <jgsheppa@protonmail.com>
2023-12-13 22:36:55 +0100

    list entire pass instead of only url

--- list/list.go
+++ list/list.go
@@ -11,6 +11,7 @@ import (
 type PassList struct {
 	Sites  []string
 	writer io.Writer
+	Passes []models.Pass
 }
 
 func NewList() *PassList {
@@ -27,6 +28,11 @@ func (pl *PassList) WithSites(sites []string) *PassList {
 	return pl
 }
 
+func (pl *PassList) WithPasses(passes []models.Pass) *PassList {
+	pl.Passes = passes
+	return pl
+}
+
 func (pl *PassList) PrintList() error {
 	for _, site := range pl.Sites {
 		_, err := fmt.Fprintf(pl.writer, "%s \n", site)
@@ -37,17 +43,23 @@ func (pl *PassList) PrintList() error {
 	return nil
 }
 
+func (pl *PassList) PrintPasses() error {
+	for _, pass := range pl.Passes {
+		_, err := fmt.Fprintf(pl.writer, "%d | %s | %s \n", pass.ID, pass.URL, pass.Password)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func Run(service models.PassService) (int, error) {
-	var urls []string
 	passes, err := service.Get()
 	if err != nil {
 		return 1, err
 	}
-	for _, pass := range passes {
-		urls = append(urls, pass.URL)
-	}
 
-	err = NewList().WithSites(urls).PrintList()
+	err = NewList().WithPasses(passes).PrintPasses()
 	if err != nil {
 		return 1, err
 	}
